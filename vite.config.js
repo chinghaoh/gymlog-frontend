34b67398c3719a431e8d7684e75342d9f475326c
Chinghaogit config --global user.email chinghao.hsu@hva.nlgit config --global user.name <chinghaoh@hotmail.nl>
2026-05-26 13:26:07 +0200

    feat: prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        cookieDomainRewrite: 'localhost',
-      }
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                cookieDomainRewrite: 'localhost',
+            }
+        }
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: false
     }
-  }
 })
